805303437a421c127655dc25bc6005c0352c3d76
Alcides <bmalcidesneto@gmail.com>
2024-09-18 10:22:53 -0300

    refac/clear-btn function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const clearButton = document.querySelector('.clear-btn');
 const inputs = document.querySelectorAll('.input-subcontainer');
+const inputRadio = document.querySelectorAll('#radio-container');
 
 clearButton.addEventListener('click', () => {
     inputs.forEach(input => {
         input.value = "";
     });
+
+    inputRadio.forEach( radio => {
+        radio.checked = false
+    }
+    )
 });
 
 
